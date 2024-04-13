26f5b2c3cca24d5a7fc5f71408c45bfc8e7349bc
luba2603 <lyba.komina@gmail.com>
2024-04-13 17:17:38 +0300

    fj
    thh

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
+const toggleContentByTab = () => {
+
+  let tabs = document.querySelectorAll('.products-block__tab');
+  let contents = document.querySelectorAll('.products-block__wrapper');
+
+  tabs.forEach((tab, index) => {
+
+    tab.addEventListener('click', () => {
+
+      tabs.forEach((tab_2) => {
+        tab_2.classList.remove('active');
+      })
+      contents.forEach((content) => {
+        content.classList.remove('active');
+      })
+
+
+      tab.classList.add('active');
+      contents[index].classList.add('active');
+
+    })
+
+  })
+
+}
+
+toggleContentByTab();
+
+var burger = document.querySelector('.burger');
+var mobilemenu = document.querySelector('.mobile-menu');
+var cross = document.querySelector('.cross');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  mobilemenu.classList.toggle('active');
+});
+
+cross.addEventListener('click', () => {
+  burger.classList.remove('active');
+  mobilemenu.classList.remove('active');
+});
+
+var korzina = document.querySelector('.korzina');
+var tovkorzina = document.querySelector('.tovkorzina');
+var krest = document.querySelector('.krest');
+
